5c1490756edfe58cb4709ecd8fccb3da6776142f
breadam <bilgi@breadam.com>
2014-05-16 02:44:31 +0300

    breadam

--- src/Breadam/Emberize/Emberize.php
+++ src/Breadam/Emberize/Emberize.php
@@ -82,7 +82,7 @@ class Emberize{
 	private function prepareModel(Model $model){
 		
 		if(in_array($model,$this->parents)){
-			return;
+			return false;
 		}
 		
 		array_push($this->parents,$model);
@@ -125,8 +125,8 @@ class Emberize{
 				
 				$attributes[$relationName] = $this->getModelIdentifierValue($result);
 				
-				if($this->sideload){
-					$this->prepareModel($result);
+				if($this->sideload && $this->prepareModel($result) === false){
+					return false;
 				}
 				
 			}else if($result instanceof Collection){
@@ -141,7 +141,9 @@ class Emberize{
 				
 				if($this->sideload){				
 					foreach($result as $item){
-						$this->prepareModel($item);
+						if($this->prepareModel($result) === false){
+							return false;
+						}
 					}
 				}
 			}
